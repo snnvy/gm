4b514dc3bd6b1f0b4c1c507b7f1e44ec0a0ecf22
Sonia <114437076+snnvy@users.noreply.github.com>
2022-12-08 07:38:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,21 @@ $(document).ready(function(){
 		dots:false,
 		slidesToShow:4,
 		autoplay:true,
-		speed:2500,
-		autoplaySpeed:1500,
-
+		speed:1400,
+		autoplaySpeed:2000,
+		touchThreshold: 20,
+		waitForAnimate:false,
+		variableWidth:true,
 	});
 });
+
+const closeNav = document.getElementById('closebtn');
+const openNav = document.getElementById('opennav');
+
+openNav.onclick = function () {
+  document.getElementById("mySidebar").style.width = "250px";
+}
+
+closeNav.onclick = function (){
+  document.getElementById("mySidebar").style.width = "0";
+}
